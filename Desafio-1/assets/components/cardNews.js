80e609b5b74eb7234fc0fc628e3e773f34b4136e
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-09-08 14:06:02 -0400

    feat: criando componente

---
+++ Desafio-1/assets/components/cardNews.js
@@ -0,0 +1,9 @@
+class cardNews extends HTMLElement{
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode: 'open'});
+        shadow.innerHTML = "<h1>Hello World</h1>";
+    }
+}
+customElements.define('card-news', cardNews);
