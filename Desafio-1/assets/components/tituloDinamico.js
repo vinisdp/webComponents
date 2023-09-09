f40ce3d514523f3ed74a9ad7ae6ec93ab209b7df
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-09-09 15:31:31 -0400

    feat: estilizando component

---
+++ Desafio-1/assets/components/tituloDinamico.js
@@ -0,0 +1,23 @@
+class tituloDinamico extends HTMLElement{
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode:"open"});
+
+        //base do component
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = this.getAttribute("titulo");
+
+        //estilizar component
+        const style = document.createElement("style");
+        style.textContent = `
+        h1{
+            color: red;
+        }`;
+        
+        
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+customElements.define('titulo-dinamico', tituloDinamico);
