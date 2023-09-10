294d16f113424125017048639b3b85226a893c07
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-09-09 23:54:54 -0400

    refactor: criando componentes filhos

--- Desafio-1/assets/components/cardNews.js
+++ Desafio-1/assets/components/cardNews.js
@@ -14,9 +14,22 @@ class cardNews extends HTMLElement{
         const cardLeft = document.createElement('div');
         cardLeft.setAttribute('class', 'card__left');
 
+        const autor = document.createElement('span');
+        const titulo = document.createElement('a');
+        const texto = document.createElement('p');
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(titulo);
+        cardLeft.appendChild(texto);
+
+
         const cardRight = document.createElement('div');
         cardRight.setAttribute('class', 'card__right');
         
+        const imagem = document.createElement('img');
+        imagem.setAttribute('src','/Desafio-1/assets/imagens/darthVader.jpg');
+
+        cardRight.appendChild(imagem);
 
         componentRoot.appendChild(cardLeft);
         componentRoot.appendChild(cardRight);
