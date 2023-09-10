292eaf52a0b0ccc1a45e4cba76caf62f0ea4bea5
Vinicius Silva <83310782+vinisdp@users.noreply.github.com>
2023-09-09 23:43:58 -0400

    refactor: adicionando div no componente

--- Desafio-1/assets/components/cardNews.js
+++ Desafio-1/assets/components/cardNews.js
@@ -3,11 +3,24 @@ class cardNews extends HTMLElement{
         super();
 
         const shadow = this.attachShadow({mode: 'open'});
-        shadow.innerHTML = "<h1>Hello World</h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.style());
     }
 
     build(){
+        const componentRoot = document.createElement('div');
+        componentRoot.setAttribute('class', 'card');
 
+        const cardLeft = document.createElement('div');
+        cardLeft.setAttribute('class', 'card__left');
+
+        const cardRight = document.createElement('div');
+        cardRight.setAttribute('class', 'card__right');
+        
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+        return componentRoot;
     }
 
     style(){
